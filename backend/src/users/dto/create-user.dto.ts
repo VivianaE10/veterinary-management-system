3b5c2e60427626b76733530a6953a8f108f22341
Viviana Ospina <vyvy_escobar@hotmail.com>
2026-04-09 17:53:41 -0500

    (users): creación de DTOs con validaciones

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsNotEmpty, MinLength } from 'class-validator';
+import { Rol } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
+  nombre: string;
+
+  @IsNotEmpty({ message: 'La cédula es obligatoria' })
+  cedula: string;
+
+  @IsEmail({}, { message: 'El correo no es valido' })
+  correo: string;
+
+  @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+  contraseña: string;
+
+  @IsEnum(Rol, { message: 'El rol debe ser ADMIN o USER' })
+  rol: Rol;
+}
